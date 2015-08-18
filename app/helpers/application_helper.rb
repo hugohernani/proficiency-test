3abd9f863589a579f89cd2ef9956525ae70fe12b
= <=>
2015-08-18 11:21:37 +0200

    full_title helper, backtrace_silencers...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = "")
+    base_title = "School System"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
+
+  def gravatar_for(student, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(student.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: student.name, class: "gravatar")
+  end
+
+
 end
