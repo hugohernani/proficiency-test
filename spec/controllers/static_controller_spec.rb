55db5d3a46927c59a2ed2ea2d8a437d06f9d19dc
= <=>
2015-08-18 11:06:19 +0200

    Finish first static pages definition

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -2,16 +2,24 @@ require 'rails_helper'
 
 RSpec.describe StaticController, :type => :controller do
 
+  render_views # Required to render views on controller type test
+
+  before :all do
+    @base_title = "Sistema escolar"
+  end
+
+
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to be_success
+      expect(response).to have_css('title', :text => @base_title)
     end
   end
 
-  describe "GET help" do
+  describe "GET enroll" do
     it "returns http success" do
-      get :help
+      get :enroll
       expect(response).to be_success
     end
   end
